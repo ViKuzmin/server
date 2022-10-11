91bd852c1d1f5207cc66752b11b9dd32dca56618
kuzmin <@kuzmin>
2022-10-11 12:45:01 +0300

    enable web socket

--- src/main/java/com/example/server/config/WebSocketConfig.java
+++ src/main/java/com/example/server/config/WebSocketConfig.java
@@ -2,10 +2,12 @@ package com.example.server.config;
 
 import com.example.server.ws.WebSocketHandler;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
 
 @Configuration
+@EnableWebSocket
 public class WebSocketConfig implements WebSocketConfigurer {
 
     private final WebSocketHandler webSocketHandler;
