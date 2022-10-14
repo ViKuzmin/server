4a7c65b757c56e626b55d68854a4784d31fab629
kuzmin <@kuzmin>
2022-10-14 21:35:50 +0300

    add raw emitter

---
+++ src/main/java/com/example/server/actors/Emitter.java
@@ -0,0 +1,25 @@
+package com.example.server.actors;
+
+
+import com.badlogic.gdx.utils.Json;
+import com.badlogic.gdx.utils.JsonValue;
+
+public class Emitter implements Json.Serializable {
+
+    private String id;
+
+    private float angle;
+    private float x;
+    private float y;
+    private int speed = 300;
+
+    @Override
+    public void write(Json json) {
+
+    }
+
+    @Override
+    public void read(Json json, JsonValue jsonValue) {
+
+    }
+}
